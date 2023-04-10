e4cd9d99c4e4a2ce6ee50d764f0118a1fa09c64d
artweise <art2wsxweise7ujm@gmail.com>
2023-04-10 23:25:34 +0300

    style(CreateEditMemoryModal): form styles, fixed button

--- src/components/Modals/CreateEditMemoryModal.jsx/style.js
+++ src/components/Modals/CreateEditMemoryModal.jsx/style.js
@@ -29,3 +29,36 @@ export const CloseRoundedIconStyles = {
   color: NEUTRAL_SHADES[1100],
   borderRadius: "20%",
 };
+
+export const formControlStyleDuo = {
+  width: "100%",
+  marginTop: "16px",
+  flex: 1,
+};
+
+export const DuoContainer = styled.div`
+  display: flex;
+  gap: 16px;
+`;
+
+export const FormContentContainer = styled.div`
+  width: 600px;
+  overflow-y: scroll;
+  max-height: 70vh;
+  padding-right: 8px;
+  margin-right: -16px;
+  ::-webkit-scrollbar {
+    width: 6px;
+  }
+  ::-webkit-scrollbar-track {
+    margin-block: 4px;
+    border-radius: 5px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: ${NEUTRAL_SHADES[400]};
+    border-radius: 5px;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: ${NEUTRAL_SHADES[600]}90;
+  }
+`;
